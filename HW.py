020466dda281d9d057d4a8f5009a9b4bf0d53e14
angelinamakeeva <125530626+angelinamakeeva@users.noreply.github.com>
2023-03-12 17:13:48 +0300

    hw

--- HW.py
+++ HW.py
@@ -7,9 +7,9 @@ co2 = v_fuel_galons * 20
 equiv_ethanol = v_fuel_galons * 115000
 coast_dollars = v_fuel_galons * 3
 amount_barrels_prd_RF = 3699000
-v_fuel_litrs_prd_RF = 3699000 * 19.5 / 143400000
+v_fuel_litrs_prd_RF = 3699000 * 19.5 * 3.785 / 143400000
 v_fuel_litras_Nsk_day = v_fuel_litrs_prd_RF * 1621500
-v_fuel_litras_cntr_year = amount_barrels_prd_RF * 19.5 * 365
+v_fuel_litras_cntr_year = amount_barrels_prd_RF * 19.5 * 3.785 * 365
 print(ru.v_fuel_litrs, v_fuel_litrs)
 print(ru.amount_barrels, amount_barrels)
 print(ru.co2, co2)
