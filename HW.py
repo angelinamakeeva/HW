01510ee077e306fc349d3ca1bd44cd4a81fd839f
angelinamakeeva <125530626+angelinamakeeva@users.noreply.github.com>
2023-03-13 05:30:53 +0300

    hw

--- HW.py
+++ HW.py
@@ -1,15 +1,17 @@
 import math
 import ru_local as ru
+
 v_fuel_galons = float(input())
 v_fuel_litrs = v_fuel_galons * 3.785
 amount_barrels = math.ceil(v_fuel_galons / 19.5)
 co2 = v_fuel_galons * 20
 equiv_ethanol = v_fuel_galons * 115000
 coast_dollars = v_fuel_galons * 3
-amount_barrels_prd_RF = 3699000
-v_fuel_litrs_prd_RF = 3699000 * 19.5 * 3.785 / 143400000
-v_fuel_litras_Nsk_day = v_fuel_litrs_prd_RF * 1621500
+amount_barrels_prd_RF = 3_699_000
+v_fuel_litrs_prd_RF = 3_699_000 * 19.5 * 3.785 / 143_400_000
+v_fuel_litras_Nsk_day = v_fuel_litrs_prd_RF * 1_621_500
 v_fuel_litras_cntr_year = amount_barrels_prd_RF * 19.5 * 3.785 * 365
+
 print(ru.v_fuel_litrs, v_fuel_litrs)
 print(ru.amount_barrels, amount_barrels)
 print(ru.co2, co2)
